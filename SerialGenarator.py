30ce55f08a87562dc721feaa61e977427258c0c9
Mark K <k.mark.kr@gmail.com>
2016-01-04 12:24:38 +0900

    improved to handle multiple serial files.

--- SerialGenarator.py
+++ SerialGenarator.py
@@ -3,15 +3,17 @@ import time
 import string
 import random
 import hashlib
+import os
 
 
-now = time.localtime()
+NOW = time.localtime()
+MAX_SERIAL_IN_FILE = 10
 
 
 def gen_time_stamp():
     time_stamp = \
-        u"{0:04d}/{1:02d}/{2:02d} {3:02d}:{4:02d}:{5:03d}".format(now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour,
-                                                                  now.tm_min, now.tm_sec)
+        u"{0:04d}/{1:02d}/{2:02d} {3:02d}:{4:02d}:{5:03d}".format(NOW.tm_year, NOW.tm_mon, NOW.tm_mday, NOW.tm_hour,
+                                                                  NOW.tm_min, NOW.tm_sec)
     return time_stamp
 
 
@@ -43,25 +45,76 @@ def gen_serial(time_stamp, rand_str):
     return serial_str
 
 
+def create_output_file_name():
+    file_name_list = os.listdir('.')
+
+    last_serial_file = 'serial_codes_001.txt'
+    last_output_file = 'output_codes_001.txt'
+
+    for file_name in file_name_list:
+        if file_name[:13] == 'serial_codes_' and last_serial_file < file_name:
+            last_serial_file = file_name
+            last_output_file = last_output_file[:13] + file_name[13:20]
+
+    return last_serial_file, last_output_file
+
+
+def get_some_numbers(last_serial_file):
+    temp_open = open(last_serial_file, 'r')
+    num_of_lines_of_last_file = len(temp_open.readlines())
+    temp_open.close()
+    num_of_serial_to_generate = int(sys.argv[1])
+    return num_of_lines_of_last_file, num_of_serial_to_generate
+
+
+########################################################################################################################
+########################################################################################################################
+
+
 def main():
+
     if len(sys.argv) is not 2:
         print("usage: GenerateSerial.py (number of serial)")
         sys.exit()
 
-    f_serial = open("serial_codes.txt", 'w')
-    f_output = open("output_codes.txt", 'w')
+    last_serial_file, last_output_file = create_output_file_name()
+    print('======file names=======')
+    print(last_serial_file)
+    print(last_output_file)
 
-    num_of_serial = int(sys.argv[1])
+    num_of_lines_of_last_file, num_of_serial_to_generate = get_some_numbers(last_serial_file)
+    total_write_line = 0
 
-    print('generating ' + str(num_of_serial) + ' serials and outputs')
+    f_serial = open(last_serial_file, 'a')
+    f_output = open(last_output_file, 'a')
 
-    for i in range(0, num_of_serial):
+    print('generating ' + str(num_of_serial_to_generate) + ' serials and outputs')
+
+    while total_write_line < num_of_serial_to_generate:
+
+        if num_of_lines_of_last_file is MAX_SERIAL_IN_FILE:
+            print('=======create next file=========')
+            num_of_lines_of_last_file = 0
+            f_serial.close()
+            f_output.close()
+
+            new_file_number = str(int(last_serial_file[13:16]) + 1).zfill(3)
+
+            last_serial_file = last_serial_file[:13] + new_file_number + '.txt'
+            last_output_file = last_output_file[:13] + new_file_number + '.txt'
+
+            f_serial = open(last_serial_file, 'a')
+            f_output = open(last_output_file, 'a')
+
         time_stamp = gen_time_stamp()
         rand_str = gen_rand_str()
         serial_str = gen_serial(time_stamp, rand_str)
 
         f_serial.write(time_stamp + '\t' + serial_str + '\n')
-        f_output.write(hashlib.sha224(serial_str).hexdigest() + '\n')
+        f_output.write(time_stamp + '\t' + hashlib.sha224(serial_str.encode('utf-8')).hexdigest() + '\n')
+
+        num_of_lines_of_last_file += 1
+        total_write_line += 1
 
     f_serial.close()
     f_output.close()
--- SerialVerifier.py
+++ SerialVerifier.py
@@ -1,27 +1,46 @@
 import sys
 import hashlib
+import os
 
 
 def main():
-    f_output = open("output_codes.txt", 'r')
+    input_serial = sys.argv[1]
+    encoded_input_serial = str(input_serial).encode('utf-8')
 
-    print('Begin verifying...')
+    file_name_list = os.listdir('.')
 
-    while True:
-        line = f_output.readline()
-        line = (line[:-1])
+    existing_file_name_list = []
 
-        hash_value = hashlib.sha224(sys.argv[1]).hexdigest()
+    for file_name in file_name_list:
+        if file_name[:13] == 'output_codes_':
+            existing_file_name_list.append(file_name)
 
-        if not line:
-            print('The key ' + sys.argv[1] + ' is not valid.')
-            break
+    for file_name in existing_file_name_list:
+        print('Verifying file [' + file_name + ']...')
 
-        if line == hash_value:
-            print('The key ' + sys.argv[1] + ' is valid.')
-            break
+        f_output = open(file_name, 'r')
 
-    print('')
+        while True:
+            line = f_output.readline()
+            line = (line[21:-1])
+
+            hash_value = hashlib.sha224(encoded_input_serial).hexdigest()
+
+            if line == hash_value:
+                print('The key ' + sys.argv[1] + ' is valid.')
+                f_output.close()
+                sys.exit()
+
+            if not line:
+                if file_name == existing_file_name_list[-1]:
+                    print('The key ' + sys.argv[1] + ' is not valid.')
+                    f_output.close()
+                    sys.exit()
+                else:
+                    break
+
+        f_output.close()
+        print('')
 
 if __name__ == '__main__':
     main()
